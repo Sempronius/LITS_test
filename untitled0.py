55dd4ca360d6ed962ce4c56bf5b8771e662e39b0
James Mason <jryanmason@gmail.com>
2018-11-24 15:48:01 -0600

    Made some changes

--- untitled0.py
+++ untitled0.py
@@ -7,6 +7,10 @@ look, I added a new line...
 git status
 git add -A
 git commit -m "Insert message here"
+--> if you run without -m, you'll need the following command to get out:
+    escape :wq 
+    --> esc :wq
+
 git push
 @author: Sempronius
 """
